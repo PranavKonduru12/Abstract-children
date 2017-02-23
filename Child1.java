6990e3afe8b356511a48073985e5f5e11a749110
Pranav Konduru <konduru1976@mydusd.org>
2017-02-23 11:25:21 -0800

    Second commit

---
+++ Child1.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class Child1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Child1 extends Children
+{
+    silent
+}
